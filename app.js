1816900ca991254e7e284a9fd2d4572bc0b8c65f
Sahil Prajapati <sahilprajapati882@gmail.com>
2025-05-26 01:16:15 +0530

    making CRUD operations

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hi. I am Root");
@@ -29,6 +30,29 @@ app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 });
+
+//new route
+app.get("/listings/new", async (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
+//show route:- iska purpose hai individual listing ka pura data print krana.
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+
+//create route:- isse jo new listing hm create krke unhe add kr skhte hai current listing(as a new list)
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+
 //     await sampleListing.save();
 //    console.log("sample was saved");
 //     res.send("successful testing");
